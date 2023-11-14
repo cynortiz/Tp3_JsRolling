d189a217e425d9ff11cae6d79102125f55c3cf88
Cynthia Ortiz <cynortiz21@gmail.com>
2023-11-14 17:49:22 -0300

    ejercicio 1 practico 3 de js

---
+++ 3-practicoJs/ejercicio1.js
@@ -0,0 +1,7 @@
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+
+console.log("Lista de Meses:");
+for (let i = 0; i < meses.length; i++) {
+    console.log(`${i + 1}. ${meses[i]}`);
+}
