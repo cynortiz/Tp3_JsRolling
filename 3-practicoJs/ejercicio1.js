21b05d86f3572b29cae0e577669bccf324a788f6
Cynthia Ortiz <cynortiz21@gmail.com>
2023-11-15 12:11:00 -0300

    realice el ejercicio 1

--- 3-practicoJs/ejercicio1.js
+++ 3-practicoJs/ejercicio1.js
@@ -1,7 +1,14 @@
+
 const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 
-console.log("Lista de Meses:");
+const ul = document.createElement("ul");
+
 for (let i = 0; i < meses.length; i++) {
-    console.log(`${i + 1}. ${meses[i]}`);
+    const li = document.createElement("li");
+    li.textContent = meses[i];
+    ul.appendChild(li);
 }
+
+document.body.appendChild(ul);
+
